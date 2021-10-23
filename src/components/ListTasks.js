2040eefa45386ddaba35f0ea17ed710b96b12e86
albertassi88 <albertassi88@gmail.com>
2021-10-23 10:37:26 -0300

    criacao do estilo ListTasks

--- src/components/ListTasks.js
+++ src/components/ListTasks.js
@@ -1,30 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import RemoveTask from  './RemoveTask';
+import "../styles/ListTasks.css";
 
 export default function ListTasks(props) {
   const { listTasks, setListTasks } = props;
   return (
-    <ul>
-      {listTasks.map((e, index) => (
-        <div key={index} className="box-tasks">
-          <li key={index}>{e}</li>
-          <label htmlFor="pomodoro">Pomodoros:</label>
-          <select id="pomodoro">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-          </select>
-          <button 
-            className="btnremove" 
-            onClick={() => RemoveTask(index, setListTasks, listTasks)}
-          >
-            Remover
-          </button>
-        </div>
-      ))}        
-    </ul>
+    <div className="container-list-tasks">
+      <ul>
+        {listTasks.map((e, index) => (
+          <div key={index} className="box-tasks">
+            <li key={index}>{e}</li>
+            <label htmlFor="pomodoro">Pomodoros:</label>
+            <select id="pomodoro">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+            </select>
+            <button 
+              className="btnremove" 
+              onClick={() => RemoveTask(index, setListTasks, listTasks)}
+            >
+              Remover
+            </button>
+          </div>
+        ))}        
+      </ul>
+    </div>  
   )
 }
 
